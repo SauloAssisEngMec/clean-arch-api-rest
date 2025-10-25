9609b10ea5fe827fd3f3bc662e05a95a3112b000
saulo <sauloassis4123@gmail.com>
2025-10-25 11:21:48 -0300

    refactor: improve generic types and change id to _id

--- src/shared/domain/entities/entity.ts
+++ src/shared/domain/entities/entity.ts
@@ -1,23 +1,25 @@
-import { v4 as uuid } from 'uuid';
+import { randomUUID } from 'crypto';
+
+type EntityJSON<Props> = Required<{ id: string } & Props>;
 
 export abstract class Entity<Props = any> {
     public readonly _id: string;
     public readonly props: Props;
 
     constructor(props: Props, id?: string) {
-        this._id = id || uuid();
+        this._id = id || randomUUID();
         this.props = props;
     }
 
     get id() {
-        return this.id;
+        return this._id;
     }
 
-    toJSON(): Required<{ id: string } & Props> {
+    toJSON(): EntityJSON<Props> {
         return {
             id: this._id,
             ...this.props,
-        } as Required<{ id: string } & Props>;
+        } as EntityJSON<Props>;
     }
 
     // public equals(entity: Entity<Props>): boolean {
